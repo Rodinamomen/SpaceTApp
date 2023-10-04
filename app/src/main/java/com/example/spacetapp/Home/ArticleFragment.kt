d82e0aac99d28d95037a9bd7c9d374f4f4078fd6
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2023-10-04 21:16:20 +0300

    video

--- app/src/main/java/com/example/spacetapp/Home/ArticleFragment.kt
+++ app/src/main/java/com/example/spacetapp/Home/ArticleFragment.kt
@@ -5,15 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.spacetapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class ArticleFragment : Fragment() {
+    private lateinit var btnNext: FloatingActionButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_article, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnNext = view.findViewById(R.id.to_video_btn)
+
+        btnNext.setOnClickListener {
+            findNavController().navigate(R.id.action_articleFragment_to_videoFragment)
+        }
     }
 }
