abd1401f0668ef84ed8abc7635270384fadb21e3
b2b <Rodinamobark3@gmail.com>
2023-10-05 21:37:39 +0300

    spinner

---
+++ app/src/main/java/com/example/spacetapp/Home/mapFragment.kt
@@ -0,0 +1,20 @@
+package com.example.spacetapp.Home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.example.spacetapp.R
+
+class mapFragment : Fragment() {
+    lateinit var next_img : ImageView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+       // next_img= view.findViewById(R.id.)
+    }
+
+
+}
