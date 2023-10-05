0a6c460baf8aa0c59f55d7412044589230c0af80
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2023-10-05 20:14:00 +0300

    reservation

--- app/src/main/java/com/example/spacetapp/Home/reservationFragment.kt
+++ app/src/main/java/com/example/spacetapp/Home/reservationFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import com.example.spacetapp.R
 
 
-class reservationFragment : Fragment() {
-    lateinit var spinner :Spinner
+class ReservationFragment : Fragment(), AdapterView.OnItemSelectedListener {
+    private val planetNames = arrayOf(
+        "Mercury", "Venus", "Earth", "Mars",
+        "Jupiter", "Saturn", "Uranus"
+    )
+    private lateinit var spinner: Spinner
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,8 +25,40 @@ class reservationFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_reservation, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        spinner= view.findViewById(R.id.spinner)
+        spinner = view.findViewById(R.id.spinner_planets)
+
+        spinner.onItemSelectedListener = this
+
+        val ad: ArrayAdapter<String> = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            planetNames
+        )
+
+        ad.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item
+        )
+
+        spinner.adapter = ad
+    }
+
+    override fun onItemSelected(
+        parent: AdapterView<*>?,
+        view: View?,
+        position: Int,
+        id: Long
+    ) {
+        Toast.makeText(
+            requireContext(),
+            planetNames[position],
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        // Handle the case where nothing is selected
     }
 }
